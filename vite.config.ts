e6fc7464937bf2ec7f64e631b3cf6725d1817f40
Asaraf Ali <asarafa411@gmail.com>
2026-09-10 23:31:55 +0530

    fix render port binding

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
-    tailwindcss(),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart(),
+    nitro(),
     react(),
   ],
 });
